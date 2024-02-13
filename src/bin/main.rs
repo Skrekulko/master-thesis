d0615519b6659cdf40c31b584399e3c65d52a094
Matej Rybár <rybm@int.govcert.cz>
2024-02-13 07:16:36 +0100

    skrekulko: ieee754

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,188 +5,7 @@ use tfhe::boolean::client_key;
 use tfhe::{prelude::*, ClientKey, FheBool, FheUint};
 use tfhe::{ConfigBuilder, generate_keys, FheInt32, FheUint32, set_server_key};
 
-use thesislib::{ieee754::*};
-
-fn uint_to_float(significand: u32) -> Option<u32> {
-    // Only support 0 < significand < 1 << 24.
-    if significand == 0 || significand >= 1 << 24 {
-        return None; // or Err("Invalid input"); or whatever you'd like here.
-    }
-
-    let mut shifts: u32 = 0;
-
-    // Align the leading 1 of the significand to the hidden-1
-    // position. Count the number of shifts required.
-    let mut temp_significand = significand;
-    while temp_significand & (1 << 23) == 0 {
-        temp_significand <<= 1;
-        shifts += 1;
-    }
-
-    // The number 1.0 has an exponent of 0, and would need to be
-    // shifted left 23 times. The number 2.0, however, has an
-    // exponent of 1 and needs to be shifted left only 22 times.
-    // Therefore, the exponent should be (23 - shifts). IEEE-754
-    // format requires a bias of 127, though, so the exponent field
-    // is given by the following expression:
-    let exponent = 127 + 23 - shifts;
-
-    // Now merge significand and exponent. Be sure to strip away
-    // the hidden 1 in the significand.
-    let merged = (exponent << 23) | (temp_significand & 0x7FFFFF);
-
-    // Return the merged value as Option<u32>.
-    Some(merged)
-}
-
-fn uint_to_float1(significand: u32) -> u32 {
-    let mut i = 0;
-    let mut shifts = 0;
-
-    let mut temp_significand = significand;
-    while temp_significand & (1 << 23) == 0 {
-        i += 1;
-        println!("i: {}", i);
-        temp_significand <<= 1;
-        shifts += 1;
-    }
-    println!("{}", shifts);
-    
-    let exponent = 127 + 23 - shifts;
-    
-    let merged = (exponent << 23) | (temp_significand & 0x7FFFFF);
-    
-    return merged;
-}
-
-fn uint_to_float2(significand: u32) -> u32 {
-    let mut shifts = significand & (1 << 23);
-    let first_is_zero: bool = shifts == 0;
-    let mut zero_found: bool = first_is_zero;
-    println!("{:032b}", significand);
-    println!("{:032b}", (1 << 23));
-    println!("{:032b}", shifts);
-    println!("shifts: [{:2}] first_is_zero: [{: ^5}] zero_round: [{: ^5}]", shifts, first_is_zero, zero_found);
-
-    // if first_is_zero {
-    for i in 0..23 {
-        let temp_significand = (significand << i) & (1 << 23);
-        println!("{:032b}", temp_significand);
-        let is_zero = temp_significand == 0;
-        if is_zero {
-            shifts += zero_found as u32 & first_is_zero as u32 & 1;
-        } else {
-            zero_found = is_zero;
-            shifts += zero_found as u32;
-        }
-        println!("i: [{:2}] is_zero: [{: ^5}] zero_round: [{: ^5}] shifts: [{:2}]", i, is_zero, zero_found, shifts);
-    }
-    // }
-
-    // let exponent = 127 + 23 - shifts;
-    // let exponent = (150 - shifts as i32) as u32;
-    let exponent: u32 = ((-(shifts as i32)) + 150) as u32;
-
-    let merged = (exponent << 23) | ((significand << shifts) & 0x7FFFFF);
-    // println!("output: {:032b}", merged);
-
-    return merged;
-}
-
-// fn uint_to_float3(significand: u32) -> u32 {
-//     let mut shifts = ((significand & (1 << 23)) != 0) as u32;
-//     let mut zero_found = true; // TODO:
-
-//     for i in 1..24 {
-//         let is_zero = ((significand << i - 1) & (1 << 23)) == 0;
-//         if is_zero {
-//             shifts += 1 & zero_found as u32;
-//         } else {
-//             zero_found = zero_found & !is_zero;
-//             shifts += 0;
-//         }
-//     }
-
-//     // let exponent = 127 + 23 - shifts;
-//     // let exponent = (150 - shifts as i32) as u32;
-//     let exponent: u32 = ((-(shifts as i32)) + 150) as u32;
-
-//     return (exponent << 23) | ((significand << shifts) & 0x7FFFFF);
-// }
-
-fn u32_to_ieee754(significand: u32) -> u32 {
-    let mut shifts = significand & (1 << 23);
-    let first_is_zero: bool = shifts == 0;
-    let mut zero_round: bool = !first_is_zero;
-    println!("shifts: [{:2}] first_is_zero: [{: ^5}] zero_round: [{: ^5}]", shifts, first_is_zero, zero_round);
-
-    for i in 0..23 {
-        let mut is_zero = ((significand << i) & (1 << 23)) == 0;
-        if first_is_zero == false {
-            is_zero = first_is_zero;
-        }
-        
-        if is_zero {
-            zero_round = zero_round;
-        }
-        else {
-            zero_round = !is_zero;
-        }
-
-        if is_zero {
-            shifts += !zero_round as u32;
-        } else {
-            shifts += !zero_round as u32;
-        }
-        
-        // println!("i: [{:2}] is_zero: [{: ^5}] zero_round: [{: ^5}] shifts: [{:2}]", i, is_zero, zero_round, shifts);
-    }
-
-    let mut sig = significand.clone();
-    let mut con = 0;
-    for i in 0..24 {
-        println!("i = {}", i);
-        let mut is_zero = ((sig.clone() << i) & (1 << 23)) == 0;
-        if is_zero {
-            con += 1;
-        } else {
-            break
-        }
-    }
-    println!("{}", con);
-
-    let exponent = (-(shifts as i32) + 150) as u32;
-
-    return (exponent << 23) | ((significand << shifts) & 0x7FFFFF);
-}
-
-fn fheuint32_to_ieee754(significand: &FheUint32, client_key: &ClientKey) -> FheUint32 {
-    let mut shifts: FheUint32 = significand.clone() & (1 << 23);
-    let first_is_zero: FheBool = shifts.eq(0);
-
-    shifts = first_is_zero.if_then_else(&calculate_shifts_uint(&significand, &shifts, &first_is_zero, &client_key), &shifts);
-
-    let exponent: FheUint32 = -shifts.clone() + 150;
-    let ushifts: FheUint32 = shifts.cast_into();
-    return (exponent << 23u32) | ((significand.clone() << ushifts) & 0x7FFFFF);
-}
-
-fn calculate_shifts_uint(significand: &FheUint32, shifts: &FheUint32, flag: &FheBool, client_key: &ClientKey) -> FheUint32 {
-    let mut cloned_shifts: FheUint32 = shifts.clone();
-    // let mut zero_round: FheUint32 = (!(flag.clone())).cast_into();
-    let mut zero_round: FheUint32 = !shifts.clone();
-    let decrypted_zero_round: u32 = shifts.decrypt(&client_key);
-    println!("{}", decrypted_zero_round);
-
-    for i in 0..23 {
-        println!("i = {}", i);
-        let is_zero: FheBool = (((significand << i as u32) & (1 << 23))).eq(0);
-        zero_round = is_zero.if_then_else(&zero_round, &(!is_zero.clone()).cast_into());
-        cloned_shifts += !zero_round.clone();
-    }
-
-    return cloned_shifts;
-}
+use thesislib::ieee754::*;
 
 fn find_m_recursive(x: u32, m:u32) -> u32 {
     if 2u32.pow(2 * m) <= x {
@@ -282,22 +101,6 @@ fn fsqrt64(mut expo: u64, mant: u64, fact: u64) -> u64 {
 }
 
 /*
-    IEEE 754 Extraction
-*/
-
-fn ieee754_extract_sign(input: u32) -> u32 {
-    return input >> 31;
-}
-
-fn ieee754_extract_exponent(input: u32) -> u32 {
-    return (input >> 23) - 127;
-}
-
-fn ieee754_extract_mantissa(input: u32) -> u32 {
-    return input & 0x7FFFFF | 0x800000;
-}
-
-/*
     Main
 */
 
@@ -309,10 +112,8 @@ fn main() {
     // Parsing
     println!("\nParsing:");
     println!("numb: [u32] {: ^20} [x] {:#018x} [b] {:064b}", numb, numb, numb);
-    let ieee: u32 = u32_to_ieee754(numb);
+    let ieee: u32 = u32_to_ieee754_2nd(numb);
     println!("ieee: [u32] {: ^20} [x] {:#018x} [b] {:064b}", ieee, ieee, ieee);
-    let ieee2: u32 = uint_to_float1(numb);
-    println!("ieee: [u32] {: ^20} [x] {:#018x} [b] {:064b}", ieee2, ieee2, ieee2);
     let sign: u32 = ieee754_extract_sign(ieee);
     println!("sign: [u32] {: ^20} [x] {:#018x} [b] {:064b}", sign, sign, sign);
     let expo: u32 = ieee754_extract_exponent(ieee);
@@ -369,19 +170,12 @@ fn main() {
     println!("decn: [u32] {: ^20} [x] {:#018x} [b] {:064b}", decrypted_number, decrypted_number, decrypted_number);
 
 
-    let encrypted_ieee754 = fheuint32_to_ieee754(&encrypted_number, &client_key);
+    let encrypted_ieee754 = fheuint32_to_ieee754(&encrypted_number);
     let decrypted_ieee754:u32 = encrypted_ieee754.decrypt(&client_key);
 
     println!("decry output: {}", decrypted_ieee754);
     println!("decry output: {:#01x}", decrypted_ieee754);
     println!("decry output: {:032b}", decrypted_ieee754);
-
-
-    // Homomorphic IEEE 754
-    println!("\nHomomorphic IEEE 754:");
-    let h_ieee: u32 = u32_to_ieee754(numb);
-    println!("ieee: [u32] {: ^20} [x] {:#018x} [b] {:064b}", h_ieee, h_ieee, h_ieee);
-
 
     // test signed
     let test: i32 = numb.cast_into();
--- src/ieee754.rs
+++ src/ieee754.rs
@@ -1,25 +1,137 @@
 use tfhe::{prelude::*, FheBool, FheInt32, FheUint32};
 
+/*
+    IEEE 754 Extraction
+*/
+
+pub fn ieee754_extract_sign(input: u32) -> u32 {
+    return input >> 31;
+}
+
+pub fn ieee754_extract_exponent(input: u32) -> u32 {
+    return (input >> 23) - 127;
+}
+
+pub fn ieee754_extract_mantissa(input: u32) -> u32 {
+    return input & 0x7FFFFF | 0x800000;
+}
+
+/*
+*   u32
+*/
+
+// The original algorithm
+pub fn u32_to_ieee754(significand: u32) -> Option<u32> {
+    // Only support 0 < significand < 1 << 24.
+    if significand == 0 || significand >= 1 << 24 {
+        return None; // or Err("Invalid input"); or whatever you'd like here.
+    }
+
+    let mut shifts: u32 = 0;
+
+    // Align the leading 1 of the significand to the hidden-1
+    // position. Count the number of shifts required.
+    let mut temp_significand = significand;
+    while temp_significand & (1 << 23) == 0 {
+        temp_significand <<= 1;
+        shifts += 1;
+    }
+
+    // The number 1.0 has an exponent of 0, and would need to be
+    // shifted left 23 times. The number 2.0, however, has an
+    // exponent of 1 and needs to be shifted left only 22 times.
+    // Therefore, the exponent should be (23 - shifts). IEEE-754
+    // format requires a bias of 127, though, so the exponent field
+    // is given by the following expression:
+    let exponent = 127 + 23 - shifts;
+
+    // Now merge significand and exponent. Be sure to strip away
+    // the hidden 1 in the significand.
+    let merged = (exponent << 23) | (temp_significand & 0x7FFFFF);
+
+    // Return the merged value as Option<u32>.
+    Some(merged)
+}
+
+// The modified algorithm
+pub fn u32_to_ieee754_2nd(input: u32) -> u32 {
+    let mut shifts: u32 = input.clone() & (1 << 23);
+    let zero_found: bool = shifts == 0;
+
+    if zero_found {
+        shifts = u32_calculate_shifts(input, shifts);
+    }
+
+    let exponent = (-(shifts as i32) + 150) as u32;
+    return (exponent << 23) | ((input << shifts) & 0x7FFFFF);
+}
+
+fn u32_calculate_shifts(input: u32, shifts: u32) -> u32 {
+    let mut cloned_shifts: u32 = shifts.clone();
+    let mut zero_found: bool = shifts == 0;
+
+    for i in 0..24 {
+        let is_zero: bool = ((input.clone() << i as u32) & (1 << 23)) == 0;
+        zero_found = zero_found & is_zero;
+        let zero_found_number: u32 = zero_found.clone().cast_into();
+        cloned_shifts += zero_found_number;
+    }
+
+    return cloned_shifts;
+}
+
+/*
+*   FheInt32
+*/
+
 pub fn fheint32_to_ieee754(input: &FheInt32) -> FheInt32 {
     let mut shifts: FheInt32 = input.clone() & (1 << 23);
-    let first_is_zero: FheBool = shifts.eq(0);
+    let zero_found: FheBool = shifts.eq(0);
 
-    shifts = first_is_zero.if_then_else(&calculate_shifts(&input, &shifts).cast_into(), &shifts);
+    shifts = zero_found.if_then_else(&fheint32_calculate_shifts(&input, &shifts).cast_into(), &shifts);
 
     let exponent: FheInt32 = -shifts.clone() + 150;
     let ushifts: FheUint32 = shifts.cast_into();
     return (exponent << 23u32) | ((input.clone() << ushifts) & 0x7FFFFF);
 }
 
-pub fn calculate_shifts(input: &FheInt32, shifts: &FheInt32) -> FheInt32 {
+fn fheint32_calculate_shifts(input: &FheInt32, shifts: &FheInt32) -> FheInt32 {
     let mut cloned_shifts: FheInt32 = shifts.clone();
-    let mut zero_round: FheBool = shifts.eq(0);
+    let mut zero_found: FheBool = shifts.eq(0);
 
     for i in 0..24 {
         let is_zero: FheBool = (((input.clone() << i as u32) & (1 << 23))).eq(0);
-        zero_round = zero_round & is_zero;
-        let zero_found_number: FheInt32 = zero_round.clone().cast_into();
+        zero_found = zero_found & is_zero;
+        let zero_found_number: FheInt32 = zero_found.clone().cast_into();
         cloned_shifts += zero_found_number;
+    }
+
+    return cloned_shifts;
+}
+
+/*
+*   FheUint32
+*/
+
+pub fn fheuint32_to_ieee754(significand: &FheUint32) -> FheUint32 {
+    let mut shifts: FheUint32 = significand.clone() & (1 << 23);
+    let zero_found: FheBool = shifts.eq(0);
+
+    shifts = zero_found.if_then_else(&fheuint32_calculate_shifts(&significand, &shifts), &shifts);
+
+    let exponent: FheUint32 = -shifts.clone() + 150;
+    let ushifts: FheUint32 = shifts.cast_into();
+    return (exponent << 23u32) | ((significand.clone() << ushifts) & 0x7FFFFF);
+}
+
+fn fheuint32_calculate_shifts(significand: &FheUint32, shifts: &FheUint32) -> FheUint32 {
+    let mut cloned_shifts: FheUint32 = shifts.clone();
+    let mut zero_found: FheUint32 = !shifts.clone();
+
+    for i in 0..23 {
+        let is_zero: FheBool = (((significand << i as u32) & (1 << 23))).eq(0);
+        zero_found = is_zero.if_then_else(&zero_found, &(!is_zero.clone()).cast_into());
+        cloned_shifts += !zero_found.clone();
     }
 
     return cloned_shifts;
